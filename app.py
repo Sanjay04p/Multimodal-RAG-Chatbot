898f05790ecf3cde7a5319ab332c6b642b29687e
Sanjay P <sanjayp0412005@gmail.com>
2026-03-23 23:08:34 +0530

    update loaders.py for different file formats

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ import shutil
 from loaders import build_corpus
 from embed_store import build_vectorstore
 from rag import rag_chat
-
+from PIL import Image 
 # Page Config
-st.set_page_config(page_title="Multimodal RAG Assistant", layout="wide")
-
+im=Image.open("logo.png")
+st.set_page_config(page_title="Multimodal RAG", page_icon="im",layout="wide")
+st.logo("logo.png",size="large")  
 def save_uploaded_files(uploaded_files):
     """
     Saves uploaded Streamlit files to a temporary directory 
@@ -22,7 +23,7 @@ def save_uploaded_files(uploaded_files):
     return temp_dir
 
 def main():
-    st.title("📄 Multimodal RAG Chatbot")
+    st.title("📚 Multimodal RAG Chatbot")
     st.markdown("Chat with your PDFs, Images, Videos, and Text files.")
 
     # Initialize Chat History
